3c516770d35be19915478bc7874c0e40506e44a1
Marilise Morona <marymorona@gmail.com>
2022-11-29 19:16:29 -0300

    Layout chat

--- WhatsAppClone/ViewController/ChatsViewController.swift
+++ WhatsAppClone/ViewController/ChatsViewController.swift
@@ -9,22 +9,60 @@ import UIKit
 
 class ChatsViewController : UIViewController {
     
-    
+    lazy var tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(ChatTableViewCell.self, forCellReuseIdentifier: "ChatTableViewCell")
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.separatorStyle = .singleLine
+        tableView.separatorColor = .red
+        return tableView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
-//        setUp()
+        view.backgroundColor = .systemPink
+        setUp()
+        reloadTableView()
     }
-//    private func setUp() {
-//        view.addSubview(<#nameComponente#>)
-//        constraints()
-//    }
-//    private func constraints() {
-//        NSLayoutConstraint.activate([
-//            <#nameComponente#>.topAnchor.constraint(equalTo: view.topAnchor),
-//            <#nameComponente#>.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            <#nameComponente#>.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//            <#nameComponente#>.bottom.constraint(equalTo: view.bottomAnchor)
-//        ])
-//    }
+    private func setUp() {
+        view.addSubview(tableView)
+        constraints()
+        protocolsTableView()
+    }
+    private func constraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    private func protocolsTableView(){
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    private func reloadTableView(){
+        self.tableView.reloadData()
+    }
+}
+extension ChatsViewController: UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var row: Int = 8
+        return row
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatTableViewCell.identifier, for: indexPath) as? ChatTableViewCell else { return UITableViewCell() }
+        var model = ChatModel(title: "Eu", message: "Ultima mensagem do dia", date: "sexta-feira", numb: "100", image: "user01")
+        cell.backgroundColor = .yellow
+        cell.populate = model
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
 }
